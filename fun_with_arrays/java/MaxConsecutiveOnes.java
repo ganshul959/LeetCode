10bffcc69b48120da95836f28b357fd39e9b0ad1
Anshulg <anshulg@nextgen.com>
2020-11-14 14:24:56 +0530

    #2 Max Consecutive Ones Problem - Solution

--- fun_with_arrays/java/MaxConsecutiveOnes.java
+++ fun_with_arrays/java/MaxConsecutiveOnes.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
 
-        int lastMaxConsecutive = 0;
+        int lastMaxConsecutive = 0; // Always assign relevant variable names
         int currentMaxConsecutive = 0;
 
 
         for(int i=0; i< nums.length; i++) {
-            if(nums[i] == 1) {
+            if(nums[i] == 1) {  // Give spacing is a good practise
                 currentMaxConsecutive++;
             }
             if(nums[i] == 0) {
@@ -16,6 +16,6 @@ class Solution {
                 currentMaxConsecutive = 0;
             }
         }
-        return currentMaxConsecutive > lastMaxConsecutive ? currentMaxConsecutive : lastMaxConsecutive;
+        return currentMaxConsecutive > lastMaxConsecutive ? currentMaxConsecutive : lastMaxConsecutive; // Eliminating one if codition and saving n times stack memory
     }
 }
